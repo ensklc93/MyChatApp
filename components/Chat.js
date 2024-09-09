0305cdb4b09701d2b18adc5dae7798f717388555
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-09-09 15:12:16 +0200

    Added GiftedChat component
    Added basic functions of GiftedChat
    Added placeholder system message
    Added basic bubble styling

--- components/Chat.js
+++ components/Chat.js
@@ -1,26 +1,79 @@
-import { StyleSheet, View, Text } from "react-native"
-import { useEffect } from "react"
+import { StyleSheet, KeyboardAvoidingView, Platform, View } from "react-native"
+import { useEffect, useState } from "react"
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
-const Chat = ({ route, navigation}) => {
+
+const Chat = ({ route, navigation }) => {
   const { name } = route.params
   const {backgroundColor} = route.params
+  const [messages, setMessages] = useState([]);
+
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+
+  const renderBubble = (props) => {
+    return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#3ec2e2"
+        },
+        left: {
+          backgroundColor: "#d0cbcb"
+        }
+      }}
+    />
+  }
+ 
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
+
 
   useEffect(() => {
     navigation.setOptions({ title: name })
-  }, [])
+  }, []);
 
   return (
     <View style={[styles.container, {backgroundColor}]}>
-      <Text style={{color: '#ffffff', fontSize: 16,fontWeight: 600}}>Let's start chatting {name}!</Text>
+      <GiftedChat
+        renderBubble={renderBubble}
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
+      {/* Added keyboard overlap fix for older Android versions */}
+      {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
     </View>
   )
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: 'white'
   }
 })
 
